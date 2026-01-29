990f1e0d992e86847465538006cc4a9b385a2d19
Alan Duff <alan.k.duff@gmail.com>
2026-01-29 20:56:12 +0000

    [FEATURE] Update Terraform configuration: add AWS resources and dependencies

--- terraform.tf
+++ terraform.tf
@@ -2,11 +2,13 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
+  /*
   cloud {
     workspaces {
       name = "learn-terraform-dependencies"
     }
   }
+  */
 
   required_providers {
     aws = {
